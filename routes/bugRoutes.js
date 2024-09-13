15d9fcf336c303152f7c5d9af906d97bddfc813a
pouiks <v.joinville@uxco-group.com>
2024-09-13 14:18:13 +0200

    adding rules for uxco sheet

--- routes/bugRoutes.js
+++ routes/bugRoutes.js
@@ -11,7 +11,7 @@ router.get('/domain/:domainName', verifyToken, getBugsByDomain); // Vérifiez qu
 router.get('/', verifyToken, getAllBugs);
 
 // Route pour créer un nouveau bug (accessible uniquement aux administrateurs)
-router.post('/', verifyToken, createBug);
-// router.post('/',  createBug);
+// router.post('/', verifyToken, createBug);
+router.post('/',  createBug);
 
 module.exports = router;
