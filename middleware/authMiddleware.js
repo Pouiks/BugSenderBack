616addc5a81facd49e8fc798d663813ee8171682
Pouiks <virgilejoinville@gmail.com>
2024-09-29 15:40:53 +0200

    suppression of doublon

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,8 +3,6 @@
 const jwt = require('jsonwebtoken');
 
 // middlewares/authMiddleware.js
-const jwt = require('jsonwebtoken');
-
 exports.verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
