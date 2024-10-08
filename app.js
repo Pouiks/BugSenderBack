7e68132c763a490f2cfc02548ad87e17032ea244
pouiks <virgilejoinville@gmail.com>
2024-10-08 09:08:58 +0200

    adding online extension ID

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'chrome-extension://bcjoahmihhljlhomedlplhpbhjlpckhc'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
